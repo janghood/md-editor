42dec4fb0a85e9d0c8ecb4102a07ad620e569c25
qunbotop <yinqunbo@126.com>
2021-07-24 13:13:57 +0800

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MilkdownEditor />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import {MilkdownEditor} from './components/MilkdownEditor'
+
+export default defineComponent({
+  components: { MilkdownEditor }
+})
+</script>
+
+<style>
+</style>
